fe97d23c39f31428d16887d506362716d82fea27
Jatin Satare <jatinsatare@gmail.com>
2025-12-09 19:26:45 +0530

    Added project asset

---
+++ index.js
@@ -0,0 +1,25 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+let countStr;
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function reset(){
+    countStr = "";
+    count= 0;
+
+    countEl.textContent = 0
+    saveEl.textContent = "Previous entries: ";
+
+}
